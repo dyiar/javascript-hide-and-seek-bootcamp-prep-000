7f773cb83b3df1d501abaeade8e1bfc3795b0161
Austin Blake <ummwhere@hotmail.com>
2017-12-08 21:11:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-
+/*
 function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.rankedLists')
 
@@ -15,6 +15,18 @@ function increaseRankBy(n) {
 
     for (let j = 0, k = children.length; j < k; j++) {
      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+} */
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
     }
   }
 }
