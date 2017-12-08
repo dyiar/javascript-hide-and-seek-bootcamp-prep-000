3b1da72120b344ff01a5073f8134541cdd512afe
Austin Blake <ummwhere@hotmail.com>
2017-12-08 21:13:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-/*
+
 function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.rankedLists')
+  const rankedLists = document.querySelectorAll('.ranked-lists')
 
   for (let i = 0, l = rankedLists.length; i<l; i++) {
     let children = rankedLists[i].children;
@@ -17,19 +17,8 @@ function increaseRankBy(n) {
      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
     }
   }
-} */
+} 
 
-function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list')
-
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children
-
-    for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n
-    }
-  }
-}
 
 function deepestChild() {
   let node = document.getElementById('grand-node')
