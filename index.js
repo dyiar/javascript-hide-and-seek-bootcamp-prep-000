147b06ffd912fdaae267783cc51582e406b5acb3
Austin Blake <ummwhere@hotmail.com>
2017-12-08 21:05:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   let node = document.getElementById('grand-node')
-  let nextNode = node.childrenp[0]
+  let nextNode = node.children[0]
 
   while (nextNode) {
     node = nextNode
